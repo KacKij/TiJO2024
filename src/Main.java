5cb775bc1a4880790234f0ad56e78ec8cec6297a
KacKij <kackij.01@gmail.com>
2024-03-08 10:52:28 +0100

    first commit test2

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         // Press Opt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome, my name Borat!");
+        System.out.printf("Hello and welcome, my name Borat asd!");
 
         }
     }
